6599d845a34b3d2d48ca6d1907a75eabbf07310c
amvc666 <fafslhwipwcl@qq.com>
2026-02-26 13:36:31 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,30 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'Public Status',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'u3334483-7cc87b0a8af702aa56d0621c',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: false,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '主站',
+      url: 'https://amvc.top/'
+    },
+    {
+    {
+      text: '联系我们',
+      url: 'https://amvc.top/about.html'
+    },
+  ],
+};
